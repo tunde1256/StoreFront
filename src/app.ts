971930c3ef80547f5647539e205abe3fcae95724
tunde1256 <ogunremitund12@gmail.com>
2025-05-07 12:12:38 +0100

    commit 1

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import categoryRoutes from './routes/category.routes';
 import productRoutes from './routes/product.routes';
@@ -9,13 +10,14 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(requestLogger); 
+app.use(requestLogger);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
 
-app.use(errorLogger); 
+app.use(errorLogger);
 
 export default app;
